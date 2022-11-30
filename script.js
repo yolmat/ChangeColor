3cf9bb01d8be61d4b63200106f44b895efd1305f
unknown <mateus.saraiva09@gmail.com>
2022-11-30 13:55:16 -0300

    page hex concluid

--- script.js
+++ script.js
@@ -3,25 +3,16 @@ const background = document.getElementById('background')
 const text = document.getElementById('text')
 
 const randomColor = () => {
-  const colors = ["red", "blue", "green", "black"];
 
-  const number = Math.floor(Math.random() * colors.length);
+  const colors = Math.floor(Math.random() * 16777215).toString(16);
 
-  let chosenColor = colors[number]
+  const colorChange = '#' + colors
 
-  if (chosenColor == "red") {
-    background.style.backgroundColor = 'red'
-    text.innerHTML = 'Red'
-  } else if (chosenColor == "blue") {
-    background.style.backgroundColor = 'blue'
-    text.innerHTML = 'Blue'
-  } else if (chosenColor == "green") {
-    background.style.backgroundColor = 'green'
-    text.innerHTML = 'Green'
-  } else if (chosenColor == "black") {
-    background.style.backgroundColor = 'black'
-    text.innerHTML = 'Black'
-  }
+  background.style.backgroundColor = colorChange
+
+  text.innerHTML = colorChange
 };
+
+randomColor()
 
 button.addEventListener("click", randomColor);
